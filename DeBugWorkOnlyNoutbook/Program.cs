feb06a978e20f511d623e1952b6f6825e32ca757
Halkman <halk-man@mail.ru>
2022-02-20 12:24:59 +0500

    13/2

--- DeBugWorkOnlyNoutbook/Program.cs
+++ DeBugWorkOnlyNoutbook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Diagnostics;
 using System.Text;
 using BenchmarkDotNet.Attributes;
@@ -10,36 +11,109 @@ namespace DeBugWorkOnlyNoutbook
 
     class Programm
     {
+
         class User
-        {
-            public string Login { get; set; }
-            public string Name { get; set; }
-            public bool IsPremium { get; set; }
-        }
-        static void ShowAds()
-        {
-            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
-            // Остановка на 1 с
-            Thread.Sleep(1000);
-
-            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
-            // Остановка на 2 с
-            Thread.Sleep(2000);
-
-            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
-            // Остановка на 3 с
-            Thread.Sleep(3000);
-        }
+      
         static void Main()
         {
-            UseString(70000);
+            var arrayList = new ArrayList()
+           {
+               1,
+               "Андрей ",
+               "Сергей ",
+               300,
+           };
+            int sumN = 0 ;
+            StringBuilder text = new StringBuilder();
+            foreach(var el in arrayList)
+            {
+                if(el is int)
+                {
+                    sumN += (int)el;
+                }
+                if (el is string s)
+                {
+                    text.Append(el);
+                }
+            }
+            var res = new ArrayList() { sumN, text.ToString() };
+            foreach (var n in res)
+            {
+                Console.WriteLine(n);
+            }
+
+            var months = new[]
+            {
+                "Jan", "Feb", "Mar", "Apr", "May" , "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            };
+
+            var numbers = new[]
+            {
+                1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12
+            };
+            var combiarray = new ArrayList();
+            foreach(var n in numbers)
+            {
+                combiarray.Add(months[n - 1]);
+                combiarray.Add(n);
+            }
+            foreach (var com in combiarray)
+                Console.Write($"{com}, " );
+            Console.WriteLine("---------------------");
+
+            var list = new ArrayList() { 2, "Lol" };
+            list.Add(2.3);
+            list.Add(44);
+            list.AddRange(new string[] { "Helo", "Word" });
+            foreach (var o in list)
+                Console.WriteLine(o);
+            Console.WriteLine("---------------------");
+            list.Add("again");
+            foreach (var o in list)
+                Console.WriteLine(o);
+            Console.WriteLine("---------------------");
+            var slice = list.GetRange(4, 3);
+            foreach (var o in slice)
+                Console.WriteLine(o);
+            Console.WriteLine("---------------------");
+            Console.WriteLine("---------------------");
+            var arlist = new ArrayList()
+            {
+                1,
+                "Dima",
+                300,
+                4.5f
+            };
+            foreach (var i in arlist)
+                Console.WriteLine(i);
+            Console.WriteLine("---------------------");
+            int FirstEl = (int)arlist[0];
+            Console.WriteLine(FirstEl);
+            string TwoEl = (string)arlist[1];
+            Console.WriteLine(TwoEl);
+
+            foreach (var i in arlist)
+                Console.WriteLine(i);
+            arlist[0] = "Kate";
+            arlist[1] = 111;
+            Console.WriteLine("---------------------");
+            foreach (var i in arlist)
+                Console.WriteLine(i);
+
+
+            string text = File.ReadAllText("C:/Users/Дмитрий/Desktop/cdev_Text.txt");
+            char[] delimiters = { ' ', '\r', '\n' };
+            var word = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            //Console.WriteLine(word.Length);
+
+            /* UseString(70000);
             Console.WriteLine("Ready to switch");
             Console.ReadKey();
 
             UseStringBuilder(70000);
             Console.WriteLine("Ready to end");
             Console.ReadKey();
-
+*/
 
 
 
@@ -64,12 +138,12 @@ namespace DeBugWorkOnlyNoutbook
                         //Console.WriteLine($"{matrix[j]}");
                     }
                 }
-                
+
             }
 
             static void Estimate(int n)
             {
-               // var timer = new Stopwatch();
+                // var timer = new Stopwatch();
                 //timer.Start();
 
                 for (int i = 0; i < n; i++)
@@ -78,7 +152,7 @@ namespace DeBugWorkOnlyNoutbook
 
                     CreateMatrix(10000);
 
-                   // timer.Stop();
+                    // timer.Stop();
                     //Console.WriteLine(timer.ElapsedMilliseconds);
                 }
             }
