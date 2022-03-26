071fc6d791edb75ee1b47f47ab55c98101f03668
Halkman <halk-man@mail.ru>
2022-03-26 14:15:59 +0500

    adapter

--- DeBugWorkOnlyNoutbook/Program.cs
+++ DeBugWorkOnlyNoutbook/Program.cs
@@ -6,6 +6,7 @@ using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using System.Collections.Generic;
 using System.Linq;
+using AdapterRealExample.Devices;
 
 namespace DeBugWorkOnlyNoutbook
 {
@@ -16,7 +17,15 @@ namespace DeBugWorkOnlyNoutbook
        
         static void Main()
         {
+            var imageDrawer = new ImageDrawer();
 
+            PaperPrinter paperPrinter = new PaperPrinter();
+
+            imageDrawer.DrawWith(paperPrinter);
+            
+            CanvasPainter canvasPainter = new CanvasPainter();
+
+            IPrinter imagePrinter = new CanvasPainterToPrinterAdapter(canvasPainter);
            
         }
 
